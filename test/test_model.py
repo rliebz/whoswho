60ca090e03abd7432fb34a259ac46abf639973a2
Robert Liebowitz <rliebz@gmail.com>
2014-07-11 19:25:15 -0400

    Add basic tests and gitignore.

---
+++ test/test_model.py
@@ -0,0 +1,21 @@
+import unittest
+from whoswho.model import Name
+
+
+class TestFullNames(unittest.TestCase):
+
+    def setUp(self):
+        self.name = Name('Robert Evan Liebowitz')
+
+    def test_name_and_initials(self):
+        self.assertEqual(self.name, 'R Evan Liebowitz')
+        self.assertEqual(self.name, 'R. Evan Liebowitz')
+        self.assertEqual(self.name, 'Robert E Liebowitz')
+        self.assertEqual(self.name, 'Robert E. Liebowitz')
+        self.assertEqual(self.name, 'R E Liebowitz')
+        self.assertEqual(self.name, 'R. E. Liebowitz')
+        self.assertEqual(self.name, 'R.E. Liebowitz')
+
+    def test_wrong_number_initials(self):
+        self.assertNotEqual(self.name, 'Robert E. E. Liebowitz')
+        self.assertNotEqual(self.name, 'R.E.E. Liebowitz')
