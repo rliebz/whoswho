3423d847139ca0d99132c0a47ddb297246a7251f
Robert Liebowitz <rliebz@gmail.com>
2014-08-02 23:56:55 -0400

    Add tests for ratio matching.

--- test/test_who.py
+++ test/test_who.py
@@ -11,7 +11,7 @@ from nameparser.config.titles import TITLES as NAMEPARSER_TITLES
 from nameparser.config.suffixes import SUFFIXES as NAMEPARSER_SUFFIXES
 
 
-class TestFullNames(unittest.TestCase):
+class TestMatch(unittest.TestCase):
 
     def setUp(self):
         self.name = 'Robert Evan Liebowitz'
@@ -79,6 +79,93 @@ class TestFullNames(unittest.TestCase):
         assert_false(who.match(name, 'Robert Evan'))
         assert_false(who.match(name, 'Evan Liebowitz',
                                options={'check_nickname': False}))
+
+
+class TestRatio(unittest.TestCase):
+
+    def setUp(self):
+        self.name = 'Robert Evan Liebowitz'
+
+    def test_string(self):
+        # Only relevant for python 2.X
+        assert_equal(who.ratio(self.name, str('Robert Liebowitz')), 100)
+
+    def test_unicode(self):
+        name = self.name
+        assert_equal(who.ratio(name, 'attaché Robert Evan Liebowitz'), 100)
+        assert_equal(who.ratio(name, 'Rōbért Èvān Lîęböwitz'), 100)
+        assert_less(who.ratio(name, 'Rōbért Èvān Lęîböwitz'), 100)
+
+    def test_name_and_initials(self):
+        assert_equal(who.ratio(self.name, 'R. Evan Liebowitz'), 100)
+        assert_equal(who.ratio(self.name, 'Robert E. Liebowitz'), 100)
+        assert_equal(who.ratio(self.name, 'R. E. Liebowitz'), 100)
+
+    def test_different_number_initials(self):
+        assert_equal(who.ratio(self.name, 'Robert Liebowitz'), 100)
+        assert_equal(who.ratio(self.name, 'R. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'Robert E. E. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'R. E. E. Liebowitz'), 100)
+        assert_equal(who.ratio('R.E.E. Liebowitz', 'R. E. E. Liebowitz'), 100)
+
+    def test_different_initials(self):
+        assert_less(who.ratio(self.name, 'E. R. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'E. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'R. V. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'O. E. Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'E. R. Liebowitz'),
+                    who.ratio(self.name, 'E. E. Liebowitz'))
+        assert_less(who.ratio(self.name, 'E. R. Liebowitz'),
+                    who.ratio(self.name, 'R. R. Liebowitz'))
+        assert_less(who.ratio(self.name, 'E. R. Liebowitz'),
+                    who.ratio(self.name, 'E. Liebowitz'))
+
+    def test_short_names(self):
+        assert_true(who.ratio(self.name, 'Rob Liebowitz'))
+        assert_less(who.ratio(self.name, 'Bert Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'Robbie Liebowitz'), 100)
+        assert_less(who.ratio(self.name, 'xxxxx Liebowitz'),
+                    who.ratio(self.name, 'Bobby Liebowitz'))
+
+    def test_suffixes(self):
+        name = 'Robert Liebowitz Jr'
+        assert_equal(who.ratio(name, 'Robert Liebowitz'), 100)
+        assert_equal(who.ratio(name, 'Robert Liebowitz Jr'), 100)
+        assert_equal(who.ratio(name, 'Robert Liebowitz, PhD'), 100)
+        assert_false(who.ratio(name, 'Robert Liebowitz, Sr'))
+        assert_false(who.ratio(name, 'Robert Liebowitz, Sr, PhD'))
+        assert_equal(who.ratio(name, 'Robert Liebowitz, Jr, PhD'), 100)
+        # Suffix doesn't change a match
+        assert_equal(who.ratio(name, 'Zachary Liebowitz, Jr'),
+                     who.ratio(name, 'Zachary Liebowitz'))
+
+    def test_equivalent_suffixes(self):
+        name = 'Robert Liebowitz Jr'
+        assert_equal(who.ratio(name, 'Robert Liebowitz Jnr'), 100)
+        assert_false(who.ratio(name, 'Robert Liebowitz Snr'))
+
+    def test_titles(self):
+        name = 'Mr. Robert Liebowitz'
+        assert_equal(who.ratio(name, 'Robert Liebowitz'), 100)
+        assert_equal(who.ratio(name, 'Sir Robert Liebowitz'), 100)
+        assert_equal(who.ratio(name, 'Dr. Robert Liebowitz'), 100)
+        assert_false(who.ratio(name, 'Mrs. Robert Liebowitz'))
+        # Title doesn't change a match
+        assert_equal(who.ratio(name, 'Dr. Zachary Liebowitz'),
+                     who.ratio(name, 'Zachary Liebowitz'))
+
+    def test_nickname(self):
+        name = 'Robert "Evan" Liebowitz'
+        assert_equal(who.ratio(name, 'Evan Liebowitz'), 100)
+        assert_equal(who.ratio('Evan Liebowitz', name), 100)
+        assert_less(who.ratio(name, 'Wrongbert Lieobwitz'), 100)
+        assert_less(who.ratio(name, 'Robert Evan'), 100)
+        assert_less(who.ratio(name, 'Evan Liebowitz',
+                              options={'check_nickname': False}), 100)
+        assert_less(who.ratio(name, 'xxxx Liebowitz'),
+                    who.ratio(name, 'xvax Liebowitz'))
+        assert_equal(who.ratio(name, 'xxxx Liebowitz'),
+                     who.ratio(name, 'xvax Liebowitz', 'strict'))
 
 
 class TestConfig(unittest.TestCase):
